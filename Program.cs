62204d3356b3209382700123c4b75ddaa22e85e3
charmed <cutqqq@mail.ru>
2022-09-17 23:39:58 +0500

    комент13

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Введите  первое число: ");
+string input1 = Console.ReadLine();
+int number1 = int.Parse(input1);
+
+Console.WriteLine("Введите  второе число: ");
+string input2 = Console.ReadLine();
+int number2 = int.Parse(input2);
+if (number1 > number2)
+{
+    Console.Write("max=");
+    Console.WriteLine(number1);
+    Console.Write("min=");
+    Console.WriteLine(number2);
+}
+else if (number1 < number2)
+{
+    Console.Write("max=");
+    Console.WriteLine(number2);
+    Console.Write("min=");
+    Console.WriteLine(number1);
+}
